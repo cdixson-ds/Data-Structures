39671dd00f381110d97b10558768682294401ef8
cdixson-ds <crystal.dixson@gmail.com>
2020-09-09 14:37:40 -0700

    Add files via upload

---
+++ Data-Structures/queue/code_practice.py
@@ -0,0 +1,27 @@
+# Print out each element of the following array on a separate line:
+# ['Joe', 2, 'Ted', 4.98, 14, 'Sam', 'void *', '42', 'float', 'pointers', 5006]
+# You may use whatever programming language you'd like.
+# Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework while going through your thought process.
+
+llist = ['Joe', 2, 'Ted', 4.98, 14, 'Sam', 'void *', '42', 'float', 'pointers', 5006]
+
+for i in llist:
+    print(i)
+
+# Print out each element of the following array on a separate line, but this time the input array can contain arrays nested to an arbitrarily deep level:
+# ['Bob', 'Slack', ['reddit', '89', 101, ['alacritty', '(brackets)', 5, 375]], 0, ['{slice, owned}'], 22]
+# For the above input, the expected output is:
+# Bob
+# Slack
+# reddit
+# 89
+# 101
+# alacritty
+# (brackets)
+# 5
+# 375
+# 0
+# {slice, owned}
+# 22
+# You may use whatever programming language you'd like.
+# Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework while going through your thought process.
